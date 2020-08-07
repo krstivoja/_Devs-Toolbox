6e4f17d8c04ab26d22da62e7431dffda2784a562
krstivoja <mailme@markokrstic.com>
2020-08-07 07:33:42 +0200

    main php update
    Added function to clear the cache

--- custom_theme.php
+++ custom_theme.php
@@ -21,24 +21,39 @@ if ( ! defined( 'WPINC' ) ) {
 // That is purchased files and I included it for personal projects since I have licence for it.
 // If you want to use it please purchase your own licence on: https://greensock.com/ 
 
+define('CP_THEME_URL', plugin_dir_url(__FILE__));
+define('CP_THEME_DIR', plugin_dir_path(__FILE__));
+
+function asset_version($path)
+{
+    $file = CP_THEME_DIR . $path;
+    if (is_file($file)) {
+        return filemtime($file);
+    }
+
+    return null;
+}
 
 
 /*==============================
 =            Styles            =
 ==============================*/
-function theme_styles() {
-	
+// Website Styles
+add_action('wp_enqueue_scripts', function () {
+    // Do not add styles on the Oxygen builder UI
+    if (defined("SHOW_CT_BUILDER") && !defined("OXYGEN_IFRAME")) return;
+
 	// Lightbox
     wp_enqueue_style( 'glightbox_css', plugin_dir_url( __FILE__ ) . 'node_modules/glightbox/dist/css/glightbox.min.css', array(), null );
 
     // Slider
     wp_enqueue_style( 'slider_css', plugin_dir_url( __FILE__ ) . 'node_modules/flickity/dist/flickity.min.css', array(), null );
 
-    // Theme CSS
-  	wp_enqueue_style( 'theme_css', plugin_dir_url( __FILE__ ) . 'assets/css/theme.css', array(), null );
-}
-add_action( 'wp_enqueue_scripts', 'theme_styles',  1000, 1 ) ;
+    // Theme CSS    
+    wp_enqueue_style('theme_css', CP_THEME_URL . 'assets/css/theme.css', [], asset_version('assets/css/theme.css'));
 
+
+}, 1000, 1);
 
 /*========================================================
 =            Scripts loaded on front end only            =
